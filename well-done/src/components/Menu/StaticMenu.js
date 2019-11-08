f8047ae9b4abfac5990ae2dc5d4a9cb84671761b
Reese Kunz <reesekunz21@gmail.com>
2019-11-08 14:30:07 -0700

    nav bar static menu styling

--- well-done/src/components/Menu/StaticMenu.js
+++ well-done/src/components/Menu/StaticMenu.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Menu.scss";
+import "./StaticMenu.scss";
 import {
   IoIosHome,
   // IoIosCreate
@@ -9,37 +9,57 @@ import {
   IoIosSettings
 } from "react-icons/io";
 import { Route, Link } from "react-router-dom";
+// import { WellDoneLogo } from "../../Images/WellDoneLogo.png";
 
-const Menu = props => {
+const StaticMenu = props => {
   console.log("props in menu", props);
   return (
-    <div>
-      <p className="well-done">WellDone</p>
+    <div className="entire-menu">
+      <img
+        className="logo"
+        src="https://res.cloudinary.com/dfulxq7so/image/upload/v1572403214/1ff21a300da2c00f0432c0b516f8492a_lzdqay.png"
+        alt="Well Done logo"
+      />
+      <p className="name">WellDone</p>
       <p className="email-address">WellDone@WellDone.org</p>
+      <span></span>
       <div className="overlay-content">
         <div className="each-nav">
           <IoIosHome size={25} />
-          <Link exact to="/dashboard" activeClassName="activeNavButton">
+          <Link
+            exact
+            to="/dashboard"
+            activeClassName="activeNavButton"
+            className="link"
+          >
             Home
           </Link>
         </div>
         <div className="each-nav">
           {/* <IoIosCreate size={25} /> */}
           <IoMdGrid size={25} />
-          <Link to="/monitors" activeClassName="activeNavButton">
+          <Link
+            to="/monitors"
+            activeClassName="activeNavButton"
+            className="link"
+          >
             Monitors
           </Link>
         </div>
         <div className="each-nav">
           {/* <IoIosKey size={25} /> */}
           <IoMdKey size={25} />
-          <Link to="/admin" activeClassName="activeNavButton">
+          <Link to="/admin" activeClassName="activeNavButton" className="link">
             Admin
           </Link>
         </div>
         <div className="each-nav">
           <IoIosSettings size={25} />
-          <Link to="/settings" activeClassName="activeNavButton">
+          <Link
+            to="/settings"
+            activeClassName="activeNavButton"
+            className="link"
+          >
             Settings
           </Link>
         </div>
@@ -48,4 +68,4 @@ const Menu = props => {
   );
 };
 
-export default Menu;
+export default StaticMenu;
--- well-done/src/pages/Settings/Settings.js
+++ well-done/src/pages/Settings/Settings.js
@@ -3,12 +3,12 @@ import EditNameForm from "../../components/SettingsForms/EditName";
 import EditEmail from "../../components/SettingsForms/EditEmail";
 import EditPassword from "../../components/SettingsForms/EditPassword";
 // import Menu from "../../components/Menu/Menu.component";
-import Menu from "../../components/Menu/Menu";
+import StaticMenu from "../../components/Menu/StaticMenu";
 
 const Settings = () => {
   return (
     <div className="settings">
-      <Menu />
+      <StaticMenu />
       <EditNameForm />
       <EditEmail />
       <EditPassword />
