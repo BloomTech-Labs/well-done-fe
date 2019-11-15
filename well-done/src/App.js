f134ddf06c15484762cd4b04bee28e03e7d776e2
Jason Prince <jasoncprince@gmail.com>
2019-11-14 21:10:04 -0700

    Resolved git conflicts and page loads now

--- well-done/src/App.js
+++ well-done/src/App.js
@@ -1,53 +1,51 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
-import AxiosWithAuth from './components/AxiosWithAuth/axiosWithAuth'
+import AxiosWithAuth from "./components/AxiosWithAuth/axiosWithAuth";
 import Landing from "./pages/Landing.jsx";
 import Dashboard from "./pages/Dashboard";
-import MonitorDetails from './pages/MonitorDetail'
-import Grid from "./components/Grid/Aggrid"
+import MonitorDetails from "./pages/MonitorDetail";
+import Grid from "./components/Grid/Aggrid";
 import Monitors from "./pages/MonitorsPage";
 
 function App() {
-  const [searchFiltered, setSearchFiltered] = useState([])
-  const [sensors, setSensors] = useState([])
+  const [searchFiltered, setSearchFiltered] = useState([]);
+  const [sensors, setSensors] = useState([]);
 
   useEffect(() => {
     AxiosWithAuth()
-    .get("https://welldone-db.herokuapp.com/api/sensors")
-    .then(res => 
-        {
-            console.log('get all sensors', res.data)
-            setSensors(res.data)
-        }
-    )
-  }, [ ])
+      .get("https://welldone-db.herokuapp.com/api/sensors")
+      .then(res => {
+        console.log("get all sensors", res.data);
+        setSensors(res.data);
+      });
+  }, []);
 
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Landing} />
-        <Route 
-          path="/dashboard" 
+        <Route
+          path="/dashboard"
           render={props => {
-            return <Dashboard {...props} 
-                        searchFiltered={searchFiltered} 
-                        setSearchFiltered={setSearchFiltered}
-                        sensors={sensors}
-                    />
-                    
-          }} 
+            return (
+              <Dashboard
+                {...props}
+                searchFiltered={searchFiltered}
+                setSearchFiltered={setSearchFiltered}
+                sensors={sensors}
+              />
+            );
+          }}
         />
-        <Route 
-          path="/monitorDetails" 
+        <Route
+          path="/monitorDetails"
           render={props => {
-            return <MonitorDetails {...props} 
-                        sensors={sensors}
-                    />
-          }} 
+            return <MonitorDetails {...props} sensors={sensors} />;
+          }}
         />
-        <Route path="/grid" component={Grid}/>
-        <Route path="/hello" component={Monitors}/>
-        <Route path="/settings" component={Settings} />
+        <Route path="/grid" component={Grid} />
+        <Route path="/hello" component={Monitors} />
+        {/* <Route path="/settings" component={Settings} /> */}
       </Switch>
     </div>
   );
