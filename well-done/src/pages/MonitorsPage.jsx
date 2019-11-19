182650b6a68cd1c1158ada09e66ddd146ef5c336
LaVon Mejia <lavonmejia@gmail.com>
2019-11-18 19:58:35 -0500

    added static menu to grid page

--- well-done/src/pages/MonitorsPage.jsx
+++ well-done/src/pages/MonitorsPage.jsx
@@ -3,13 +3,14 @@ import Grid from "../components/Grid/Aggrid";
 import Monitors from "../components/Monitors";
 import Menu from "../components/Menu/Menu.component"
 import "./MonitorsPage.scss"
+import StaticMenu from "../components/Menu/StaticMenu"
 
 const MonitorsPage = () => {
   return (
 <div className="parent-div">
 
     <div className="hamburger-menu"> 
-    <Menu/>
+    <StaticMenu/>
     </div>
     <div className="monitorspage">
     <Monitors/>
