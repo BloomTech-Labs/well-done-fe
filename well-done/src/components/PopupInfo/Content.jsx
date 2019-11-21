6c5096d5a4a015f95a0290e00a220380334ca0c8
ren.meta@gmail.com <ren.meta@gmail.com>
2019-11-20 21:19:17 -0500

    added content to modal and styling finished

--- well-done/src/components/PopupInfo/Content.jsx
+++ well-done/src/components/PopupInfo/Content.jsx
@@ -5,18 +5,15 @@ export default ({ close }) => (
     <a className="close" onClick={close}>
       &times;
     </a>
-    <div className="header"> Modal Title </div>
+    <div className="header"> What do these statuses mean?</div>
     <div className="content">
       {" "}
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, a nostrum.
-      Dolorem, repellat quidem ut, minima sint vel eveniet quibusdam voluptates
-      delectus doloremque, explicabo tempore dicta adipisci fugit amet
-      dignissimos?
-      <br />
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sit
-      commodi beatae optio voluptatum sed eius cumque, delectus saepe
-      repudiandae explicabo nemo nam libero ad, doloribus, voluptas rem alias.
-      Vitae?
+      <h4>Functional</h4>
+      A sensor is currently online and providing accurate data.
+      <h4>Unknown</h4>
+      A sensor is currently not reporting and its data is unknown. 
+      <h4>Non-Functional</h4>
+      A sensor is currently not functional.
     </div>
   </div>
 );
