960a3aee2700fbd56a60c8db5ee112974828f314
Jason Prince <jasoncprince@gmail.com>
2019-11-22 09:45:26 -0700

    pulling

--- well-done/src/components/Grid/Pagination.jsx
+++ well-done/src/components/Grid/Pagination.jsx
@@ -1,11 +1,8 @@
 const gridOptions = {
-
-    onRowClicked(event) {
-        //   event.preventDefault();
-          console.log('The row was clicked.');
-        }
-
-
-    }
+  onRowClicked(event) {
+    //   event.preventDefault();
+    console.log("The row was clicked.");
+  }
+};
 
 export default gridOptions;
