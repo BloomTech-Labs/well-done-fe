5f56b8fd23cdf43ac6d237185ee5ddd49604354f
Gavin Dreyer <gavindreyer1@gmail.com>
2019-12-16 11:28:40 -0800

    initial commit

--- src/components/AxiosWithAuth/axiosWithAuth.jsx
+++ src/components/AxiosWithAuth/axiosWithAuth.jsx
@@ -1,14 +1,14 @@
-import axios from "axios";
+import axios from 'axios'
 
 const AxiosWithAuth = () => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('token')
   // console.log('token', token)
   return axios.create({
     headers: {
-      "Content-Type": "application/json",
-      Authorization: token
-    }
-  });
-};
+      'Content-Type': 'application/json',
+      Authorization: token,
+    },
+  })
+}
 
-export default AxiosWithAuth;
+export default AxiosWithAuth
