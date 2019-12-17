821eaa2c2442e608ca3cb1c2355903517423b09f
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-12-16 19:29:24 -0500

    Styled nav bar

--- src/components/Navbar/NavRight.js
+++ src/components/Navbar/NavRight.js
@@ -4,9 +4,10 @@ import { NavLink } from 'react-router-dom'
 const NavRight = () => {
   return (
     <div className='nav-right'>
-      <NavLink to='/dashboard'>Dashboard</NavLink>
-      <NavLink to='/monitors'>Monitors</NavLink>
-      <NavLink to='/organization'>Organizations</NavLink>
+      <NavLink className="margin-left" to='/dashboard'>Dashboard</NavLink>
+      <NavLink className="margin-left" to='/monitors'>Monitors</NavLink>
+      <NavLink className="margin-left margin-right" to='/organization'>Organizations</NavLink>
+      <div className="margin-left">💧</div>
     </div>
   )
 }
