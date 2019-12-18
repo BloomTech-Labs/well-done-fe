7545488ba249a22df0297ffeb34fb409c817f86c
Gavin Dreyer <gavindreyer1@gmail.com>
2019-12-18 14:21:29 -0800

    Added styling to OrgAcCards

--- src/components/DashBoardComponents/OrganizationActivityCard.js
+++ src/components/DashBoardComponents/OrganizationActivityCard.js
@@ -1,43 +1,41 @@
 import React from 'react'
+import moment from 'moment'
 
 const OrganizationActivityCard = props => {
+  let found = props.sensors.find(item => {
+    if (item.sensor_id === props.items.sensor_id) {
+      return item
+    }
+  })
+  console.log(found)
+
   const routeHandler = e => {
     e.preventDefault()
-
-    let found = props.sensors.find(item => {
-      if (item.sensor_id === props.items.sensor_id) {
-        return item
-      }
-    })
-
     props.setSelectedPump(found)
     props.history.push('/monitordetails')
   }
 
   return (
     <div className='orgActivityAlertInfo'>
-      <div className='orgActivityCardContainer'>
-        <div key={props.index} className='orgActivityCardLeft'>
-          <p>
-            <span className='orgSpan'>Current Date: </span>
-            {props.items.created_at}
-          </p>
-          <p>
-            <span className='orgSpan'>Pump ID: </span>
-            {props.items.sensor_id}
-          </p>
-        </div>
-        <div className='orgActivityCardRight'>
-          <p>
-            <span className='orgSpan'>Status: </span> {props.currentStatus}
-          </p>
-          <p>
-            <span className='orgSpan'>Previous Status: </span>
-            {props.prevStatus}
-          </p>
-        </div>
+      <div key={props.index} className='orgActivityCardContainer'>
+        <p className='pumpOrg'>
+          <span className='orgSpan'>Pump #{props.items.sensor_id}</span>
+          <span className='orgSpan'>Organization: {found.org_name}</span>
+        </p>
+        <p className='pumpOrg'>
+          <span className='orgSpan'>Status: {props.currentStatus}</span>
+          <span className='orgSpan'>Province: {found.province_name}</span>
+        </p>
+        <p className='pumpOrg'>
+          <span className='orgSpan'>Previous Status: {props.prevStatus}</span>
+
+          <span className='orgSpan'>District: {found.district_name}</span>
+        </p>
       </div>
       <span className='btnCon'>
+        <span className='date'>
+          {moment(props.items.created_at).format('MMMM Do, YYYY')}
+        </span>
         <button
           onClick={e => routeHandler(e, props.data)}
           className='btn btn-info'
