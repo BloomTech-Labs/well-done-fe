34fa68380c05893886589ee3f4a0d0441f8044e5
Taylor <52665999+taylorhh5@users.noreply.github.com>
2020-01-09 16:15:47 -0500

    Changed nav logo and styled dropdown

--- src/components/Navbar/NavLeft.js
+++ src/components/Navbar/NavLeft.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './NavBar.scss'
+import DropletDroplet from '../../icons/DropletDroplet.svg'
 
 const NavLeft = () => {
-  return <div className='nav-left'>   <img
+  return <div className='nav-left'>  
+  <span className="logo-text">WellDone</span> <img
   className="logo"
-  src="https://res.cloudinary.com/dfulxq7so/image/upload/v1572403214/1ff21a300da2c00f0432c0b516f8492a_lzdqay.png"
+  src={DropletDroplet}
   alt="Well Done logo"
 /></div>
 }
