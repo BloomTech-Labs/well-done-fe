b4d2656b82fc96146408b3e2a90643bac2e361d5
Luis Martinez <mrluismartinezzz@gmail.com>
2020-01-13 10:16:05 -0800

    defined accountGridColumn

---
+++ src/components/Grid/accountGrid/accountGridColumn.js
@@ -0,0 +1,77 @@
+export const accountColumns = [{
+    headerName: 'id',
+    field: 'id',
+    sortable: true,
+    filter: true,
+    width: 60,
+    cellStyle: {
+      'font-size': '2rem',
+      'padding-top': '.75rem',
+    },
+  },
+  {
+    headerName: 'Organization',
+    field: 'organization',
+    sortable: true,
+    filter: true,
+    width: 150,
+    cellStyle: {
+      'font-size': '2rem',
+      'padding-top': '.75rem',
+    },
+  },
+  {
+    headerName: 'First Name',
+    field: 'first_name',
+    sortable: true,
+    filter: true,
+    width: 120,
+    cellStyle: {
+      'font-size': '2rem',
+      'padding-top': '.75rem',
+    },
+  },
+  {
+    headerName: 'Last Name',
+    field: 'last_name',
+    sortable: true,
+    filter: true,
+    width: 120,
+    cellStyle: {
+      'font-size': '2rem',
+      'padding-top': '.75rem',
+    },
+  },
+  {
+    headerName: 'Email',
+    field: 'email_address',
+    sortable: true,
+    filter: true,
+    width: 200,
+    cellStyle: {
+      'font-size': '2rem',
+      'padding-top': '.75rem',
+    },
+  },
+  {
+    headerName: 'Mobile',
+    field: 'mobile_number',
+    sortable: true,
+    filter: true,
+    width: 150,
+    cellStyle: {
+      'font-size': '2rem',
+      'padding-top': '.75rem',
+    },
+  },
+  {
+    headerName: 'Role',
+    field: 'role',
+    sortable: true,
+    filter: true,
+    width: 150,
+    cellStyle: {
+      'font-size': '2rem',
+      'padding-top': '.75rem',
+    },
+  }]
