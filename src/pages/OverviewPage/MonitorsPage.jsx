d3d38e06646628308aaefaaca44879e60b15f89b
Taylor <52665999+taylorhh5@users.noreply.github.com>
2020-01-16 18:45:37 -0500

    Styled status cards in monitors page

--- src/pages/OverviewPage/MonitorsPage.jsx
+++ src/pages/OverviewPage/MonitorsPage.jsx
@@ -9,6 +9,7 @@ import Grid from "../../components/Grid/Aggrid"
 import PercentageChart from "./PercentageChart"
 import Menu from '../../components/Menu/Menu.component'
 import Sensors from '../../components/DashBoardComponents/Sensors'
+import './MonitorsPage.scss'
 
 // ant design style
 import { Row, Col, Layout } from 'antd'
@@ -41,59 +42,40 @@ const MonitorsPage = (props) => {
     )
   }, [sensorSelector.isFetching])
 
-  return (
-    <div>
-      {/* <Layout style={{ backgroundColor: "#E5E5E5" }}>
-        <Content> */}
-          {/* legend */}
-          {/* <Row type="flex" justify="start"> */}
-          {/* <div className='dash-mob'> 
-        <Menu  history={props.history} /></div> */}
-            {/* <Col span={23} offset={1}> */}
-              <Legend />
-            {/* </Col>
-          </Row> */}
-
-          {/* Cards */}
-          {/* <Row type='flex' justify='space-around'>
-            <Col span={20}> */}
-              <StatusCards
-                pumpData={pumpData}
-                funcPumps={funcPumps}
-                unPumps={unPumps}
-                nonPumps={nonPumps}
-              />
-            {/* </Col>
-          </Row> */}
-
-          {/* Grid */}
-          {/* <Row type='flex' justify='space-between'>
-            <Col
-              span={10}
-              offset={1}
-              style={{ maxWidth: '600px', minWidth: '270px' }}
-            > */}
-              {/* <Grid sensors={sensorSelector.sensors} /> */}
-            {/* </Col> */}
-
-            {/* Percentage Chart */}
-            {/* <Col span={13} style={{ minWidth: '270px' }}> */}
+  return ( 
+    <>
+    <div className='monitorContainer'>
+         <div className='percentContainer'>
               <PercentageChart
                 funcPumps={funcPumps}
                 unPumps={unPumps}
                 nonPumps={nonPumps}
               />
-            {/* </Col> */}
-          {/* </Row>
-        </Content>
-      </Layout> */}
+              </div>
+      
+          {/* legend */}
+          {/* <div className='dash-mob'> 
+        <Menu  history={props.history} /></div> */}
+          <div className='cardsContainer'>
+              <StatusCards
+                pumpData={pumpData}
+                funcPumps={funcPumps}
+                unPumps={unPumps}
+                nonPumps={nonPumps}
+                />
+        
+      </div>
+      </div>
+      <div className='sensorTable'>
       <Sensors
             gridInfo={sensorSelector.gridInfo}
             selectedPump={props.selectedPump}
             setSelectedPump={props.setSelectedPump}
           />
-    </div>
+   </div>
+    </>
   )
 }
+
 
 export default MonitorsPage
