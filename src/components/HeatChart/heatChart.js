db3acd0d05f9d5fe578e3ed53355b33125ebd7fb
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2020-01-22 10:10:40 -0600

    Update heatChart.js

--- src/components/HeatChart/heatChart.js
+++ src/components/HeatChart/heatChart.js
@@ -9,16 +9,20 @@ import './heatChartStyles.scss';
 import { connect } from 'react-redux';
 
 const HeatChart = props => {
+<<<<<<< Updated upstream
 function shiftDate(date, numDays) {
   const newDate = new Date(date);
   newDate.setDate(newDate.getDate() + numDays);
   return newDate;
 }
+=======
+>>>>>>> Stashed changes
 
 function getRange(count) {
   return Array.from({ length: count }, (_, i) => i);
 }
 
+<<<<<<< Updated upstream
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -26,10 +30,20 @@ function getRandomInt(min, max) {
 
 const today = new Date();
 
+=======
+const today = new Date();
+
+function shiftDate(date, numDays) {
+  const newDate = new Date(date);
+  newDate.setDate(newDate.getDate() + numDays);
+  return newDate;
+}
+>>>>>>> Stashed changes
 
 const statusHistory = props.history.filter(day => {
   return day.sensor_id === props.selectedPump.physical_id
 })
+<<<<<<< Updated upstream
 //   this is generating values TODO pull in values (1,2 or 3 from history)
     const randomValues = getRange(360).map(index => {
       // console.log(today);
@@ -66,6 +80,43 @@ const statusHistory = props.history.filter(day => {
       <ReactTooltip />
     </div>
   );
+=======
+
+const {status, sensor_pid}
+= props.selectedPump
+
+const 
+return( 
+<div className='calendarBox'>
+<CalendarHeatmap
+  startDate={shiftDate(today, -360)}
+  endDate={today}
+  
+  values = {statusHistory.map(day =>
+    day.status == null|| day.status == null || day.status ==0 ?(
+      
+    ))}
+
+  classForValue={value => {
+    if (!value) {
+      return 'color-empty';
+    }
+    return `color-github-${value.count}`;
+  }}
+  tooltipDataAttrs={value => {
+    return {
+      'data-tip': `${value.date.toISOString().slice(0, 10)} has count: ${
+        value.count
+      }`,
+    };
+  }}
+  showWeekdayLabels={true}
+  onClick={value => alert(`Clicked on value with count: ${value.count}`)}
+/>
+<ReactTooltip />
+</div>
+)   
+>>>>>>> Stashed changes
 }
 
 export default HeatChart
