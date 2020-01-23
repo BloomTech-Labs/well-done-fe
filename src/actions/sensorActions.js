fcbb6d25ac58d8ae47a8ccfe4edc109e13f9c2d2
Surfsol <surfsol@protonamil.com>
2020-01-23 18:35:26 -0300

    SensorSelector filter array, sensorAction create actions

--- src/actions/sensorActions.js
+++ src/actions/sensorActions.js
@@ -8,6 +8,7 @@ export const SENSOR_POST = 'SENSOR_POST'
 export const WITHOUT_HISTORY_SUCCESS = 'WITHOUT_HISTORY_SUCCESS'
 export const UPDATE_INFO_WITHOUT_HISTORY = 'UPDATE_INFO_WITHOUT_HISTORY'
 export const SENSOR_DELETE = 'SENSOR_DELETE'
+export const SENSOR_SELECTED = 'SENSOR_SELECTED'
 
 
 export const fetchSensorsWithHistory = () => dispatch => {
@@ -75,4 +76,9 @@ export const deleteSensor = sensor_index => dispatch => {
       console.log(res.data)
     })
     .catch(res => dispatch({ type: SENSOR_FAILURE, payload: res.data }))
+}
+
+//update Sensor Table
+export const sensorSelected = finalList => dispatch => {
+  dispatch({type: SENSOR_SELECTED, payload: finalList})
 }
--- src/pages/MonitorsPage/SensorSelector.js
+++ src/pages/MonitorsPage/SensorSelector.js
@@ -2,29 +2,43 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import { Slider } from 'antd';
-import gridOptionss from '../../components/Grid/Pagination'
+import {sensorSelected} from '../../actions/sensorActions'
 
-import {useSelector} from 'react-redux'
+import {useSelector, dispatch, useDispatch} from 'react-redux'
 
 const SensorSelector = () => {
 
 const sensorIds = useSelector(state => state.sensorReducer.sensors)
+const grid = useSelector(state => state.sensorReducer.gridInfo)
+console.log(`grid`,grid)
 
 const physical_id = sensorIds.map(e => {
     return e.physical_id
 })
 
-let first = physical_id[0]
+let lowest = Math.min(...physical_id)
 
-let last = physical_id[physical_id.length -1]
-console.log(last)
+let highest = Math.max(...physical_id)
 
-function onChange(value) {
-  console.log('onChange: ', value);
-}
+
+const dispatch = useDispatch()
+
+let highestList = []
+let finalList =[]
 
 function onAfterChange(value) {
   console.log('onAfterChange: ', value);
+  grid.map(e => {
+      if(e.physical_id <= value[1]){
+          return highestList.push(e)
+      }
+  })
+  highestList.map(e => {
+      if(e.physical_id >= value[0]){
+          return finalList.push(e)
+      }
+  })
+  dispatch(sensorSelected(finalList))
 }
 
 
@@ -35,16 +49,14 @@ return (
     
     <Slider
       range
-      min={first}
-      max={last}
+      min={lowest}
+      max={highest}
       step={10}
-      defaultValue={[first, last]}
-      onChange={onChange}
+      defaultValue={[lowest, highest]}
       onAfterChange={onAfterChange}
     />
   </div>
   </>
-  //document.getElementById('container'),
 );
 }
 export default SensorSelector
