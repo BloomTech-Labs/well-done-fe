fce3a192124950d3798239266d684f3ec913d1ff
Luis Martinez <mrluismartinezzz@gmail.com>
2020-01-24 14:42:48 -0800

    removed unneed code

--- src/components/HeatChart/heatChart.js
+++ src/components/HeatChart/heatChart.js
@@ -1,92 +1,83 @@
-import React from 'react';
-import 'react-calendar-heatmap/dist/styles.css';
-import CalendarHeatmap from 'react-calendar-heatmap';
-import ReactTooltip from 'react-tooltip';
-import { useDispatch, useSelector } from 'react-redux'
-import './heatChartStyles.scss';
-import moment from 'moment';
+import React from 'react'
+import 'react-calendar-heatmap/dist/styles.css'
+import CalendarHeatmap from 'react-calendar-heatmap'
+import ReactTooltip from 'react-tooltip'
+import { useSelector } from 'react-redux'
+import './heatChartStyles.scss'
+import moment from 'moment'
 
 const handleClick = value => {
-  if (value === null) return(alert(`no info`))
-   alert(`on Date:${value['date']} the status was 
-${value.count}`) 
+  if (value === null) return alert(`no info`)
+  alert(`on Date:${value['date']} the status was 
+${value.count}`)
 }
 
-const handleStatus = statCodeNum =>{
- return statCodeNum === 1 ? "Unknown": statCodeNum === 2 ?"Functional": statCodeNum === 0 || null ? "not functional":"Unknown"
+const handleStatus = statCodeNum => {
+  return statCodeNum === 1
+    ? 'Unknown'
+    : statCodeNum === 2
+    ? 'Functional'
+    : statCodeNum === 0 || null
+    ? 'not functional'
+    : 'Unknown'
 }
 
-const today = new Date();
+const today = new Date()
 const HeatChart = props => {
-
-const currentlySelected = useSelector(state => state.selectedSensors.currentlySelected)
-
-
-const statusHistoryArr = props.history.filter(day => {
-   return day.sensor_id ===
-   currentlySelected.sensor_pid
-
- }
-)
- const calVals = statusHistoryArr.map( val =>{
-  return{
-    date: val.created_at,
-    count: val.status
-  }
- }
+  const currentlySelected = useSelector(
+    state => state.selectedSensors.currentlySelected
   )
- 
- const randomValues = getRange(359).map(index => {
-  return {
-    date: shiftDate(today, -index),
-    count: getRandomInt(1, 3),
-  };
 
-   
-
-});
-
-function shiftDate(date, numDays) {
-  const newDate = new Date(date);
-  newDate.setDate(newDate.getDate() + numDays);
-  return newDate;
-}
-const { status, created_at, sensor_pid } = currentlySelected
-return( 
-<div className='calendarBox'>
-<h1>{sensor_pid}</h1>
-<CalendarHeatmap
- startDate={shiftDate(today, -359)}
- endDate={today}
- values={calVals}
-
-  classForValue={value => {
-    if (!value || null) {
-      return 'color-empty';
-    }
-    return `color-github-${value.count}`;
-  }}
-  tooltipDataAttrs={value => {
+  const statusHistoryArr = props.history.filter(day => {
+    return day.sensor_id === currentlySelected.sensor_pid
+  })
+  const calVals = statusHistoryArr.map(val => {
     return {
-      'data-tip': `${moment(value.date)} has count: ${
-        handleStatus(value.count)
-      }`,
-    };
-  }}
-  showWeekdayLabels={true}
-  onClick={value => handleClick(value) }
-/>
-<ReactTooltip />
-</div>
-)   
+      date: val.created_at,
+      count: val.status,
+    }
+  })
+
+  function shiftDate(date, numDays) {
+    const newDate = new Date(date)
+    newDate.setDate(newDate.getDate() + numDays)
+    return newDate
+  }
+  const { status, created_at, sensor_pid } = currentlySelected
+  return (
+    <div className='calendarBox'>
+      <h1>{sensor_pid}</h1>
+      <CalendarHeatmap
+        startDate={shiftDate(today, -359)}
+        endDate={today}
+        values={calVals}
+        classForValue={value => {
+          if (!value || null) {
+            return 'color-empty'
+          }
+          return `color-github-${value.count}`
+        }}
+        tooltipDataAttrs={value => {
+          return {
+            'data-tip': `${moment(value.date)} has count: ${handleStatus(
+              value.count
+            )}`,
+          }
+        }}
+        showWeekdayLabels={true}
+        onClick={value => handleClick(value)}
+      />
+      <ReactTooltip />
+    </div>
+  )
 }
 
 function getRange(count) {
-  return Array.from({ length: count }, (_, i) => i);
+  return Array.from({ length: count }, (_, i) => i)
 }
 
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 export default HeatChart
