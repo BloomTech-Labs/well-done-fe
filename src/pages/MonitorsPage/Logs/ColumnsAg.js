09c5ab5817d292e410c392a1b40b22d9e76faefd
Timothy Sharkey <Techne3@gmail.com>
2020-01-29 13:37:39 -0700

    Created agGrid table to display logs Data for selected sensors

---
+++ src/pages/MonitorsPage/Logs/ColumnsAg.js
@@ -0,0 +1,99 @@
+import React from 'react'
+import moment from 'moment'
+
+function ColumnsAg() {
+  return [
+    {
+      headerName: 'Log ID',
+      field: 'id',
+      sortable: true,
+      filter: true,
+      filter: 'agNumberColumnFilter',
+      minWidth: 95,
+      cellStyle: {
+        'font-size': '2rem',
+        'padding-top': '.75rem',
+      },
+    },
+    {
+      headerName: 'Date Filed',
+      field: 'date_filed',
+      // sortable: true,
+      filter: true,
+      minWidth: 90,
+      cellStyle: {
+        'font-size': '1.5rem',
+        'padding-top': '.75rem',
+      },
+      filter: 'agDateColumnFilter',
+      filterParams: {
+        comparator: function(filterLocalDateAtMidnight, cellValue) {
+          var dateAsString = moment(cellValue).format('DD/MM/YYYY')
+          var dateParts = dateAsString.split('/')
+          var cellDate = new Date(
+            Number(dateParts[2]),
+            Number(dateParts[1]) - 1,
+            Number(dateParts[0])
+          )
+          if (filterLocalDateAtMidnight.getTime() == cellDate.getTime()) {
+            return 0
+          }
+          if (cellDate < filterLocalDateAtMidnight) {
+            return -1
+          }
+          if (cellDate > filterLocalDateAtMidnight) {
+            return 1
+          }
+        },
+        // browserDatePicker: true
+      },
+    },
+    {
+      headerName: 'Operator',
+      field: 'operator_id',
+      sortable: true,
+      filter: true,
+      minWidth: 90,
+      cellStyle: {
+        'font-size': '1.5rem',
+        'padding-top': '.75rem',
+      },
+    },
+    {
+      headerName: 'Status',
+      field: 'status',
+      sortable: true,
+      filter: true,
+      minWidth: 90,
+      cellStyle: {
+        'font-size': '1.5rem',
+        'padding-top': '.75rem',
+      },
+    },
+
+    {
+      headerName: 'NGO',
+      field: 'org_name',
+      sortable: true,
+      filter: true,
+      minWidth: 90,
+      cellStyle: {
+        'font-size': '1.5rem',
+        'padding-top': '.75rem',
+      },
+    },
+    {
+      headerName: 'Date Completed',
+      field: 'last_modified',
+      sortable: true,
+      filter: true,
+      minWidth: 90,
+      cellStyle: {
+        'font-size': '1.5rem',
+        'padding-top': '.75rem',
+      },
+    },
+  ]
+}
+
+export default ColumnsAg
--- src/pages/MonitorsPage/Logs/LogsTable.js
+++ src/pages/MonitorsPage/Logs/LogsTable.js
@@ -1,12 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import LogsHeader from './LogsHeader'
-import { fetchLogs } from 'actions/logsActions'
+import columnAg from './ColumnsAg'
+
+//agGrid
+import gridOptionss from '../../../components/Grid/Pagination'
+import { AgGridReact } from 'ag-grid-react'
+import 'ag-grid-community/dist/styles/ag-grid.css'
+import 'ag-grid-community/dist/styles/ag-theme-balham.css'
 
 // redux
+import { fetchLogs } from 'actions/logsActions'
 import { withRouter } from 'react-router'
 import { useDispatch, useSelector } from 'react-redux'
 
 function LogsTable(props) {
+  const [gridApi, setgridApi] = useState(null)
   const logsSelector = useSelector(state => state.logsReducer)
   const selectedSensors = useSelector(
     state => state.selectedSensors.currentlySelected
@@ -21,13 +29,57 @@ function LogsTable(props) {
   const sensorFiltered = logsSelector.logsData.filter(log => {
     return log.sensor_id === selectedSensors.sensor_pid
   })
-
   console.log(sensorFiltered, 'this is the filtered logs')
 
+  let gridColumnApi
+  const onGridReady = params => {
+    setgridApi(params.api)
+    gridColumnApi = params.columnApi
+  }
+
+  const onGridSizeChanged = params => {
+    var gridWidth = document.getElementById('grid-wrapper').offsetWidth
+    var columnsToShow = []
+    var columnsToHide = []
+    var totalColsWidth = 0
+    var allColumns = params.columnApi.getAllColumns()
+    for (var i = 0; i < allColumns.length; i++) {
+      var column = allColumns[i]
+      totalColsWidth += column.getMinWidth()
+      if (totalColsWidth > gridWidth) {
+        columnsToHide.push(column.colId)
+      } else {
+        columnsToShow.push(column.colId)
+      }
+    }
+    params.columnApi.setColumnsVisible(columnsToShow, true)
+    params.columnApi.setColumnsVisible(columnsToHide, false)
+    params.api.sizeColumnsToFit()
+  }
+
   return (
-    <div>
-      <LogsHeader />
-    </div>
+    <>
+      <div id='grid-wrapper' style={{ width: '100%', height: '100%' }}>
+        <div
+          id='myGridLogs'
+          style={{
+            height: '500px',
+            width: '100%',
+          }}
+          className='ag-theme-balham'
+        >
+          <AgGridReact
+            history={props.history}
+            columnDefs={columnAg()}
+            rowData={sensorFiltered}
+            gridOptions={gridOptionss}
+            onGridSizeChanged={onGridSizeChanged}
+            onGridReady={onGridReady}
+            floatingFilter={true}
+          />
+        </div>
+      </div>
+    </>
   )
 }
 
